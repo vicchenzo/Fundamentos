301e497739c64a530ac76627174e4b6bbd9fe2d3
ᴠɪᴄ. <106128743+vicchenzo@users.noreply.github.com>
2025-02-16 23:01:22 -0300

    Create 05-exercicios.js

---
+++ JS/05-exercicios.js
@@ -0,0 +1,36 @@
+//Exercício 1: Escreva um programa simples que exiba uma string no terminal utilizando o console.log
+console.log('Olá, mundo!');
+
+// Exercício 2: Modifique o programa anterior para armazenar a mensagem em uma variável antes de exibi-la no console
+let mensagem = 'Olá, mundo!';
+console.log(mensagem);
+
+// Exercício 3: Declare três variáveis mutáveis: nome (string), idade (number) e isStudent (boolean)
+let nome = 'Alejandro';
+let idade = 36;
+let isStudent = true;
+
+// Exercício 4: Modifique o valor das variáveis anteriores e imprima os novos valores no console
+nome = 'Daniel';
+idade = 30;
+isStudent = false;
+console.log(nome, idade, isStudent)
+
+// Exercício 5: Crie uma variável constante que irá armazenar um número e imprima no console
+const numero = 36;
+console.log(numero)
+
+// Exercício 6: Declare duas variáveis, endereco e telefone, sem atribuir valores a elas
+let endereco;
+let telefone;
+
+//Exercício 7: Declare variáveis com nomes descritivos para armazenar o nome de um produto, seu preço e a quantidade em estoque
+let ProductName = 'Produto: ';
+let Price = 'Preço: ';
+let quantity = 'Quantidade: ';
+console.log(`${ProductName}\n${Price} \n${quantity}`);
+
+// Declare uma variável para armazenar o nome de uma cidade e outra para armazenar a sua população. Exiba uma mensagem combinando ambas as variáveis
+let cidade = 'Gold Coast'
+let population = 100
+console.log(`Eu vou morar na Austrália em ${cidade} e a populacao é de ${population}k habitantes.`)
