75f7c6aba399bf89270729ad263c12391e96225d
ᴠɪᴄ. <106128743+vicchenzo@users.noreply.github.com>
2025-02-16 23:00:27 -0300

    Create 03-booleanos.js

---
+++ JS/03-booleanos.js
@@ -0,0 +1,16 @@
+let ligado = true; //verdadeiro = sim = 1
+let desligado = false; //falso = nao = 0
+
+console.log(true == 1);// executando retorna true, ou seja [true = 1]
+console.log(false == 0);// [false = 0] true
+
+// Caso fosse ao contrário os valores serão totalmente diferentes
+console.log(true == 0);// executando retorna true, ou seja [true = 1]
+console.log(false == 1); // [false = 0] true
+
+// transformando variável em um booleano
+let nome = "Paulo";
+console.log(Boolean(nome));
+
+let numero = 0;
+console.log(Boolean(numero));
