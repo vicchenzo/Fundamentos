3841186b28c8c501f1debabf3e3158431c215688
ᴠɪᴄ. <106128743+vicchenzo@users.noreply.github.com>
2025-02-16 23:04:49 -0300

    Create 06-operadoresAritimeticos.js

---
+++ JS/06-operadoresAritimeticos.js
@@ -0,0 +1,27 @@
+//adicao
+let valor = 10;
+let segundoValor = 43;
+
+console.log(valor + segundoValor)
+
+// let mensagem = "20";
+// console.log(mensagem + valor)// concatenacao, unimos uma string com outro valor
+
+//subtracao
+console.log(`subtração 10 - 43 = ${valor - segundoValor}`);
+console.log(`subtração 43 - 10 = ${segundoValor - valor}`)
+
+//Divisão
+console.log(`Divisao de 43 / 10 = ${segundoValor/valor}`)
+
+//módulo = resto da divisão
+console.log(`Módulo de 10 % 43 = ${valor%segundoValor}`)
+console.log(`Módulo de 43 % 10 = ${segundoValor%valor}`)
+
+let modulo = 10 % 2
+let divisao = 10/2
+console.log(`Módulo de 10 % 2 = ${modulo}`)
+console.log(`Divisão de 10 / 2 = ${divisao}`)
+
+let x = 5%3
+console.log(x)
