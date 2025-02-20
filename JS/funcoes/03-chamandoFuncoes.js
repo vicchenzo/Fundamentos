841d9afa448768a09bf8bf2fb145310a3ba8e689
ᴠɪᴄ. <106128743+vicchenzo@users.noreply.github.com>
2025-02-19 22:42:16 -0300

    Create 03-chamandoFuncoes.js

---
+++ JS/funcoes/03-chamandoFuncoes.js
@@ -0,0 +1,14 @@
+// Chamadas de Função
+function exibirDetalhes(nome, curso) {
+  console.log('Professor: ' + nome + ' Curso: ' + curso); // instrução
+}
+// cahamada: Acao que executa código dentro da funcao
+exibirDetalhes('Victor Chavez', 'Software Engineer');
+exibirDetalhes('leBron James', 'Basketball Fundamentals');
+exibirDetalhes('Ayrton Senna', 'Formula 1 Driver');
+
+function mensagem() {
+  console.log('Imprimindo uma mensagem!');
+}
+// Chamada
+mensagem();
