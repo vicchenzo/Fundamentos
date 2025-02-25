ff03f10ea0ec81ab879d9fcbfc2cef259a8a4ac0
ᴠɪᴄ. <106128743+vicchenzo@users.noreply.github.com>
2025-02-24 22:39:44 -0300

    Create 07-funcaoExpressao.js

---
+++ JS/funcoes/07-funcaoExpressao.js
@@ -0,0 +1,11 @@
+// declaracao e expressao de funcao;
+
+
+let total = function (a, b) { // funcao anonima
+  return a + b;
+};
+
+console.log(total(3, 4));
+console.log(total(13, 4));
+console.log(total(3, 14));
+console.log(total(3, 24));
